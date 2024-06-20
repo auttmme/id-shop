d26d4d74cab680f1d9459287516c2743584c2f97
auttmme <ichautm14@gmail.com>
2024-06-20 15:03:37 +0700

    new: navbar mobile view

--- app/components/layout/UserSetting.tsx
+++ app/components/layout/UserSetting.tsx
@@ -13,44 +13,12 @@ import {
 } from "@chakra-ui/react";
 import React, { useState } from "react";
 import PrimaryBtn from "../PrimaryButton";
+import LanguageSelector from "../LanguageSelector";
 
 function UserSetting() {
-  const languages = ["Bahasa Indonesia", "English"];
-  const [language, setLanguage] = useState("Bahasa Indonesia");
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleLanguage = (language: string) => {
-    setLanguage(language);
-  };
-
   return (
     <Flex gap={4} alignItems="center" width={"100%"} justifyContent="flex-end">
-      <Menu>
-        <MenuButton
-          as={Button}
-          onClick={() => setIsOpen(!isOpen)}
-          rightIcon={isOpen ? <ChevronUpIcon /> : <ChevronDownIcon />}
-          variant="outline"
-          _hover={{ bgColor: "transparent" }}
-        >
-          <Flex gap={2}>
-            <Image
-              src={"/language-icon.svg"}
-              width={18}
-              height={18}
-              alt="language icon"
-            />
-            <Text>{language}</Text>
-          </Flex>
-        </MenuButton>
-        <MenuList>
-          {languages.map((language, index) => (
-            <MenuItem key={index} onClick={() => handleLanguage(language)}>
-              {language}
-            </MenuItem>
-          ))}
-        </MenuList>
-      </Menu>
+      <LanguageSelector />
       <Button
         variant="ghost"
         size={{ base: "sm", md: "md" }}
